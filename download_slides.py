24a9ee39a4147b1fcfaf2e0b1f7a53c2e1698ba7
c0deWiZz <57440451+c0deWiZz@users.noreply.github.com>
2020-05-09 14:54:44 +0200

    bugfix

---
+++ download_slides.py
@@ -0,0 +1,42 @@
+import os
+import requests
+
+jobs = {
+    "82515105f43940ec8136": "IBN Zettel",
+    "ff5f3ac42c724ae4940c": "IBN Folien",
+    "e1eaccb7bc614b338681": "IBN Videos",
+}
+
+url_list = "https://heibox.uni-heidelberg.de/api/v2.1/share-links/{}/dirents"
+url_file = "https://heibox.uni-heidelberg.de/d/{}/files/?p={}&dl=1"
+
+for job, folder in jobs.items():
+    os.makedirs(folder, exist_ok=True)
+    r = requests.get(url_list.format(job))
+    r.raise_for_status()
+    items = r.json()["dirent_list"]
+    for item in items:
+        if item["is_dir"]:
+            msg = "is_dir"
+            raise NotImplementedError(msg)
+        if item["file_path"] != "/" + item["file_name"]:
+            msg = "file_path"
+            raise NotImplementedError(msg)
+        filename = os.path.join(folder, item["file_name"])
+        if os.path.isfile(filename):
+            if os.path.getsize(filename) == item["size"]:
+                print("Skipping {}".format(filename))
+                continue
+        status = "\r{{: >3.0f}}% Downloading {}".format(filename)
+        print(status.format(0), end="")
+        r = requests.get(url_file.format(job, item["file_path"]), stream=True)
+        r.raise_for_status()
+        with open(filename, "wb") as fh:
+            progress = 0
+            bufsize = 1 * 10 ** 6  # 1 MB
+            for buf in r.iter_content(bufsize):
+                fh.write(buf)
+                progress += len(buf)
+                percent = progress / item["size"] * 100
+                print(status.format(percent), end="")
+            print()
