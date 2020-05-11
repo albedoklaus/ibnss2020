713103c5073ff231416fc456b6cbfdd8773d4ca6
c0deWiZz <57440451+c0deWiZz@users.noreply.github.com>
2020-05-11 18:45:12 +0200

    asd

--- IBN_2_test.c
+++ IBN_2_test.c
@@ -1,15 +1,19 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main ()
 {
 	if (fork() > 0)
 	{
 		printf("%s\n", "fork > 0");
+		printf("%ld %ld", (long)getpid(), (long)getppid());
+		printf("\n");
 		sleep(1000);
 	}
 	else
 	{
 		printf("%s\n", "else");
+		printf("%ld %ld", (long)getpid(), (long)getppid());
 		exit(0);
 	}
 	return 0;
