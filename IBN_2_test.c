8070871e44487ffd3a01f56da45a1e409e3278de
albedoklaus <49734806+albedoklaus@users.noreply.github.com>
2020-05-11 17:17:53 +0000

    Update IBN_2_test.c

--- IBN_2_test.c
+++ IBN_2_test.c
@@ -3,17 +3,39 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-int main ()
+/*int main ()
 {
 	if (fork() > 0)
 	{
 		printf("%s %ld %ld\n", "fork>0", (long)getpid(), (long)getppid());
-		sleep(1000);
+		sleep(10);
 	}
 	else
 	{
 		printf("%s %ld %ld\n", "else", (long)getpid(), (long)getppid());
 		exit(0);
+	}
+	return 0;
+}
+*/
+int main ()
+{
+	if (fork() > 0)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			printf("%s %d\n", "parent", i);
+			sleep(1);
+		}
+	}
+	else
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			printf("%s %d\n", "child", i);
+			sleep(1);
+		}
+		//exit(0);
 	}
 	return 0;
 }
