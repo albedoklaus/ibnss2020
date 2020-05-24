3897844568c083b18b4199294762468075797c98
albedoklaus <49734806+albedoklaus@users.noreply.github.com>
2020-05-24 22:10:29 +0000

    Update IBN_04_2.c

--- IBN_04_2.c
+++ IBN_04_2.c
@@ -6,44 +6,62 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_INPUT_LENGTH 1024
+#define MAX_INPUT 1024
 
 int main(void) {
 
-    int fd[2], n, i;
+    int fd[2];
+    int n;
+    int i;
     pid_t pid;
-    char zeile[MAX_INPUT_LENGTH];
+    char input[MAX_INPUT];
     char shift_value = 2;
 
     if (pipe(fd) < 0) {
-        fprintf(stderr, "Couldn't create pipe");
+        fprintf(stderr, "Error creating pipe");
     }
 
+    /* Parent process */
     if ((pid = fork()) > 0) {
 
         close(fd[0]);
-        fprintf(stdout, "Unencrypted: ");
-        fgets(zeile, MAX_INPUT_LENGTH, stdin);
-        write(fd[1], zeile, strlen(zeile));
-        if (waitpid(pid, NULL, 0) < 0)
-            fprintf(stderr, "Fehler bei waitpid()????");
-        }
+        printf("Unencrypted: ");
+        fgets(input, MAX_INPUT, stdin);
+        write(fd[1], input, strlen(input));
 
+        /* Wait for child process */
+        if (waitpid(pid, NULL, 0) < 0) {
+            perror("Error waiting for child process");
+            exit(1);
+        }
+        close(fd[1]);
+        strcpy(input, "abc\n");
+        n = read(fd[0], input, MAX_INPUT);
+        printf("%s%s", "Encrypted: ", input);
+
+    }
+
+    /* Child process */
     else {
 
         close(fd[1]);
-        n = read(fd[0], zeile, MAX_INPUT_LENGTH);
+        n = read(fd[0], input, MAX_INPUT);
+        /*sleep(3);*/
         for (i = 0; i < n; i++) {
-            if (zeile[i] == '\n') {
+            if (input[i] == '\n') {
                 /* Don't convert newline character */
                 continue;
             }
-            printf("%i\n", zeile[i]);
-            zeile[i] += shift_value;
+            printf("%i\n", input[i]);
+            input[i] += shift_value;
         }
-        fprintf(stderr, "Encrypted: ");
-        write(STDOUT_FILENO, zeile, n);
+        printf("%s%s", "Encrypted: ", input);
+        close(fd[0]);
+        write(fd[1], input, strlen(input));
+
 
     }
+
     exit(0);
+
 }
